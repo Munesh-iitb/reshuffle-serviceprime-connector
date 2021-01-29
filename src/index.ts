600687976e2a033521bdf10799771320c4488258
MSI\munesh <muneshkk1998@gmail.com>
2021-01-29 13:00:24 +0530

    Third Commit

--- src/index.ts
+++ src/index.ts
@@ -66,4 +66,7 @@ export class ServiceprimeConnector extends BaseConnector {
   public async UpddateTicket(ticket: SPTicket) {
     return this.request('POST', 'newticket', ticket)
   }
+  public async GetallTicket(ticket: SPTicket) {
+    return this.request('GET', 'newticket', ticket)
+  }
 }
