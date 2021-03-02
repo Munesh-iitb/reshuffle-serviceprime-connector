a79618039288a379e10964f144ce5c1b1db6f457
MSI\munesh <muneshkk1998@gmail.com>
2021-03-02 10:47:10 +0530

    Second Commit

--- src/index.ts
+++ src/index.ts
@@ -29,19 +29,19 @@ type Options = Record<string, any>
 export class ServiceprimeConnector extends BaseConnector {
   constructor(app: Reshuffle, private readonly options: Options = {}, id?: string) {
     super(app, options, id)
-    // Bearer Token a016b76...
+    // a016b76...
     if (typeof options.bearerToken !== 'string' || options.bearerToken.length === 0) {
       throw new Error('Invalid bearerToken in options')
     }
-    //this is for your baseUrl
+    // localhost:44300 or Base Url
     if (typeof options.server !== 'string' || options.server.length === 0) {
       throw new Error('Invalid BaseUrl in options')
     }
   }
 
-  private async request(path: string, body?: string) {
+  private async request(method: 'GET' | 'POST', path: string, body?: string) {
     const res = await fetch(`https://${this.options.server}/API/${path}`, {
-      method: 'GET',
+      method,
       headers: {
         'Authorization': `bearer ${this.options.bearerToken}`,
         'Content-Type': 'application/json',
@@ -58,12 +58,21 @@ export class ServiceprimeConnector extends BaseConnector {
   }
 
   // Actions ////////////////////////////////////////////////////////
-
   public async getTicket(ticketId: string) {
     if (!/^TSR\-\d{2}\-\d{6}$/.test(ticketId)) {
       throw new Error(`Invalid ticker ID: ${ticketId}`)
     }
-    const res = await this.request(`module/Record/${ticketId}`)
+    const res = await this.request('GET',`module/Record/${ticketId}`)
+    if (!res) {
+      throw new Error('Unnown ServicePrime error')
+    }
+    if (res.Status !== true) {
+      throw new Error(`ServicePrime Error: ${res.ErrorMessage || 'Unknown'}`)
+    }
+    return res.Data
+  }
+  public async getOpenTickets(path: string) {
+    const res = await this.request('GET',`module/Records/${path}`)
     if (!res) {
       throw new Error('Unnown ServicePrime error')
     }
